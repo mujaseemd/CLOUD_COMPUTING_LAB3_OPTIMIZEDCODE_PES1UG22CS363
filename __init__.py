c98228960e4d04b87f725dbea7a4d57bc367d308
mujaseemd <mujaseemd09@gmail.com>
2025-01-23 17:37:29 +0530

    CART

--- __init__.py
+++ __init__.py
@@ -1,26 +1,48 @@
-import cart
+import json
+from cart import dao
 import products
-from cart import get_cart
-import os
-
-def checkout(username):
-    cart = get_cart(username)
-    total = 0
-    for item in cart:
-        total += item.cost 
-
-    #Here the exit can happen when a illegal memory is accessed 
-    # or when a error is not handled properly
-    #os._exit(1)
-    return total
+from products import Product
 
 
-def complete_checkout(username):
-    cartv = cart.get_cart(username)
-    items = cartv
-    for item in items:
-        assert item.qty >= 1
-    for item in items:
-        cart.delete_cart(username)
-        products.update_qty(item.id, item.qty-1)
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
 
+    @staticmethod
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+
+
+def get_cart(username: str) -> list:
+    cart_details = dao.get_cart(username)
+    if cart_details is None:
+        return []
+    
+    # Assuming 'contents' is a JSON string or list-like object stored in the database
+    items = []
+    for cart_detail in cart_details:
+        # Directly parse the contents if it's a string representing a list or dictionary
+        contents = json.loads(cart_detail['contents']) if isinstance(cart_detail['contents'], str) else cart_detail['contents']
+        items.extend(contents)
+    
+    # Batch retrieve all products in one go to reduce multiple DB calls
+    product_ids = [item['id'] for item in items]  # Assuming item has 'id' key
+    products_list = products.get_products_by_ids(product_ids)
+    
+    # Map the evaluated contents to product objects
+    return [product for product in products_list if product.id in product_ids]
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
